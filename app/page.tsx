2f09afde2eea2f1444aaf94ca8097520be6cdbcb
omeryigitler <omeryigitler@hotmail.com>
2025-12-29 01:04:09 +0100

    V25: Differentiate Booked and Active statuses

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable @next/next/no-img-element */
 "use client";
 
-// Deployment Trigger: FORCE_V24_CLEANUP_AND_REFINEMENT_STABLE
+// Deployment Trigger: FORCE_V25_BOOKED_ACTIVE_DIFFERENTIATION_STABLE
 import { useState, useEffect, useCallback } from "react";
 import { listenToSlots, listenToUsers, bookSlotTransaction, cancelBookingTransaction, logoutUserAuth } from "./services/pilatesService";
 import { Card, CardContent } from "@/app/components/ui/card";
