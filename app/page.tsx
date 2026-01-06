725c0058614714225b8932d45d5912c060681372
omeryigitler <omeryigitler@hotmail.com>
2026-01-06 07:33:46 +0100

    v61 Retry: Triggering fresh deployment

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable @next/next/no-img-element */
 "use client";
 
-// Deployment Trigger: FORCE_V61_SPRING_BUTTERFLY_FIX
+// Deployment Trigger: FORCE_V61_RETRY
 import { useState, useEffect, useCallback } from "react";
 import { listenToSlots, listenToUsers, bookSlotTransaction, cancelBookingTransaction, logoutUserAuth, updateExpiredSlots } from "./services/pilatesService";
 import { Card, CardContent } from "@/app/components/ui/card";
